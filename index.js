ff39bb7b9c4a2c38cd93733c01b8fa5a0801df45
NefeliaLeo-hue <59597965@qq.com>
2026-08-14 03:38:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -197,18 +197,23 @@ console.log("[OST Player] INIT START");
     playerDOM.addEventListener('pointerup', endDrag);
     playerDOM.addEventListener('pointercancel', endDrag);
 
-    // =====================
-    // 缩放点击逻辑 (保持不变)
+        // =====================
+    // 缩放与恢复逻辑
     // =====================
     
-    $('#ost-min-btn').on('click', function(e) {
-        e.stopPropagation(); 
+    // 点击 🔽 按钮最小化
+    $(document).on('click', '#ost-min-btn', function(e) {
+        e.stopPropagation(); // 防止点穿
         $('#ost-player-container').addClass('minimized');
+        console.log('[OST Player] 触发最小化');
     });
 
-    $('#ost-player-container').on('click', function(e) {
+    // 点击小球恢复原本形态
+    $(document).on('click', '#ost-player-container', function(e) {
+        // 必须是 minimized 状态，且没有在被拖拽 (isMoved 为 false) 时点击才生效
         if ($(this).hasClass('minimized') && !isMoved) {
             $(this).removeClass('minimized');
+            console.log('[OST Player] 触发窗口恢复');
         }
     });
 
